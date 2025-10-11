8ceda58a55d2628ca755e2bca5b820f85bb3e3ec
Djakyz <djakyz@gmail.com>
2025-10-11 17:05:50 +0300

    init

---
+++ src/server.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import cors from 'cors';
+import PinoHttp from 'pino-http';
+import 'dotenv/config';
+
+const app = express();
+const logger = PinoHttp({
+  transport: {
+    target: 'pino-pretty',
+  },
+});
+
+app.use(cors());
+app.use(logger);
+app.use(express.json());
+
+app.get('/notes', (req, res) => {
+  res.json({
+    message: 'Retrieved all notes',
+  });
+});
+
+app.get('/notes/:id', (req, res) => {
+  res.json({
+    message: 'Retrieved note with ID: id_param',
+  });
+});
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+});
+
+app.use((error, req, res, next) => {
+  res.status(500).json({
+    message: error.message,
+  });
+});
+
+const port = Number(process.env.PORT) || 3000;
+
+app.listen(port, () => console.log(`Server is running at 3000 port`));
