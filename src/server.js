12ccea8c4bae76617e6733950a4e99a6e952dfba
Djakyz <djakyz@gmail.com>
2025-11-09 16:32:10 +0200

    add upload avatar opportunity

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import notesRoutes from './routes/notesRoutes.js';
 import { errors as celebrateErrorHandler } from 'celebrate';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.use(cookieParser());
 
 app.use('/', notesRoutes);
 app.use(authRoutes);
+app.use(userRoutes);
 
 app.use(notFoundHandler);
 app.use(celebrateErrorHandler());
